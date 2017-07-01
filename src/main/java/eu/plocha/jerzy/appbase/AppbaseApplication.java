a223b2be56f720a6927d0b2f618f27241635e965
Cl3aR <jerzy.plocha@gmail.com>
2017-07-01 11:17:49 +0200

    initial

---
+++ src/main/java/eu/plocha/jerzy/appbase/AppbaseApplication.java
@@ -0,0 +1,12 @@
+package eu.plocha.jerzy.appbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppbaseApplication.class, args);
+    }
+}
